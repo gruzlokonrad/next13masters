c31b458536901d5d467a2857c3a7db7e0b7f8b4a
Konrad Gruzło <gruzlokonrad@gmail.com>
2024-01-24 23:52:39 +0100

    refactor

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -1,9 +1,9 @@
 'use client'
+import { type Route } from 'next'
 import Link from 'next/link'
 import React from 'react'
 import clsx from 'clsx'
 import { usePathname } from 'next/navigation'
-import type { Route } from 'next'
 
 type ActiveLink = {
   href: Route,
@@ -12,11 +12,11 @@ type ActiveLink = {
   children: React.ReactNode
 }
 
-export const ActiveLink = ({href, children, className, activeClassName}: ActiveLink) => {
+export const ActiveLink = ({ href, children, className, activeClassName }: ActiveLink) => {
   const pathname = usePathname()
   const isActive = href === pathname
   return (
-    <Link href={{ pathname: href }} 
+    <Link href={{ pathname: href }}
       className={clsx(`
         button 
         ${className} 
